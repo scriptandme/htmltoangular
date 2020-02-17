7253096e44fe8a93391cea578f8998ff8159b604
Cynthia Flores <cvfloresg@gmail.com>
2020-02-16 22:30:47 -0300

    Fin sección 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path: 'home', component: PortafolioComponent},
+	{path: 'about', component: AboutComponent},
+	{path: 'item', component: ItemComponent},
+	{path: '**', pathMatch:'full', redirectTo: ''}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes, {useHash: true})], 
+	  exports: [RouterModule]
+	  /*
+	  La opción useHash: true, agrega a todas nustras url el simbolo '#',
+	  esto el navegador lo interpreta, diciendo que todas las cosas que pongamos después del slash /, 
+	  no son carpetas o sub carpetas, si no que solo URL's ::: Ejemplo: http://localhost:4200/#/home ..
+	  Nos sirve para subir nuestra app a servidores donde no podamos configurar nuestro .htacces */
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortafolioComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
